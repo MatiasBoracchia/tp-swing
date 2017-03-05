6797cd42639baa359491c68570a159142db7d35c
Matías Boracchia <matias.boracchia@ing.austral.edu.ar>
2017-03-05 15:32:23 -0300

    First commit

---
+++ src/Menu.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Matias on 3/5/17.
+ */
+public class Menu {
+}
---
+++ src/Tester.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Usuario on 03-Mar-17.
+ */
+
+public class Tester {
+
+    public static void main(String[] args){
+        Window myWindow = new Window();
+        myWindow.setVisible(true);
+    }
+
+}
