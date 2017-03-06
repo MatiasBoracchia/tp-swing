e8325876c5f546b1713f4d75fb6498afeb5ce8db
Matías Boracchia <matias.boracchia@ing.austral.edu.ar>
2017-03-06 19:32:42 -0300

    Cambios menores

--- src/views/HelloWorldSwing.java
+++ src/views/HelloWorldSwing.java
@@ -14,7 +14,7 @@ public class HelloWorldSwing {
 
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("views.HelloWorldSwing");
+        JFrame frame = new JFrame("HelloWorldSwing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Add the ubiquitous "Hello World" label.
         JLabel label = new JLabel("Hello World");
--- src/views/Window.java
+++ src/views/Window.java
@@ -21,51 +21,41 @@ public class Window extends JFrame {
         //Panels
         JPanel mainPanel = new JPanel();
         JPanel buttonPanel = new JPanel();
+        JPanel titlePanel = new JPanel();
         add(mainPanel);
 
         //Layouts
-        BorderLayout borderLayout = new BorderLayout(50, 50);
-        BoxLayout boxLayout = new BoxLayout(buttonPanel, BoxLayout.PAGE_AXIS);
+        BorderLayout borderLayout = new BorderLayout();
+        BoxLayout buttonBox = new BoxLayout(buttonPanel, BoxLayout.PAGE_AXIS);
+        BoxLayout textBox = new BoxLayout(titlePanel, BoxLayout.PAGE_AXIS);
 
-        buttonPanel.setLayout(boxLayout);
+        buttonPanel.setLayout(buttonBox);
+        titlePanel.setLayout(textBox);
         mainPanel.setLayout(borderLayout);
+        mainPanel.add(titlePanel, BorderLayout.NORTH);
         mainPanel.add(buttonPanel, BorderLayout.CENTER);
 
+        //Labels
+        JLabel title = new JLabel("Trabajo Práctico Swing");
+        JLabel subtitle = new JLabel("por Matías Boracchia y Franco Palumbo");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        subtitle.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        //Box theBox = Box.createVerticalBox();
-
+        titlePanel.add(title);
+        titlePanel.add(subtitle);
 
         //Buttons
         JButton start = new JButton("Start");
-        start.setSize(new Dimension(40,40));
-
-
-        JButton setting = new JButton("Settings");
-
+        JButton settings = new JButton("Settings");
         JButton exit = new JButton("Exit");
+        start.setAlignmentX(Component.CENTER_ALIGNMENT);
+        settings.setAlignmentX(Component.CENTER_ALIGNMENT);
+        exit.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         buttonPanel.add(start);
-        mainPanel.add(Box.createVerticalStrut(40));
-        start.setPreferredSize(new Dimension(40,40));
-
-        buttonPanel.add(setting);
-        mainPanel.add(Box.createVerticalStrut(40));
-
+        buttonPanel.add(settings);
         buttonPanel.add(exit);
-        //theBox.add(Box.createHorizontalStrut(100));
 
-
-        this.add(buttonPanel);
-
-
-        //Label
-        /*JLabel titulo = new JLabel("Titulo");
-        mainPanel.add(titulo);
-
-        mainPanel.add(start);
-        mainPanel.add(setting);
-        mainPanel.add(exit);
-*/
     }
 
 }
